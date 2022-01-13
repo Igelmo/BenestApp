98fac65b611c86abbf4b533bb1fb875ceb3da048
laia <laia.igelmo.a@gmail.com>
2022-01-13 17:58:58 +0100

    add menu and top bar to main activity.

--- app/src/main/java/com/example/benestapp/MainActivity.kt
+++ app/src/main/java/com/example/benestapp/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.benestapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.benestapp.ui.theme.BenestAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +31,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "BenestApp") },
+                backgroundColor = MaterialTheme.colors.primary,
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }) {
+                        Icon(Icons.Default.Menu, "Menu")
+                    }
+                }
+            )
+        },
+        drawerContent = {
+            Text("El teu espai")
+            Divider()
+        },
+    ) {
+        Box(Modifier.padding(it)) {
+        }
+    }
 }
 
 @Preview(showBackground = true)
