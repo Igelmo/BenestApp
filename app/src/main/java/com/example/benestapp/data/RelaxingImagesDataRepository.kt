92814d9e1030d5beb893a9019bda2dcd480322ac
laia <laia.igelmo.a@gmail.com>
2022-01-13 17:59:37 +0100

    Create relaxing images repository on domain and data

---
+++ app/src/main/java/com/example/benestapp/data/RelaxingImagesDataRepository.kt
@@ -0,0 +1,4 @@
+package com.example.benestapp.data
+
+class RelaxingImagesDataRepository {
+}
---
+++ app/src/main/java/com/example/benestapp/domain/RelaxingImagesRepository.kt
@@ -0,0 +1,5 @@
+package com.example.benestapp.domain
+
+interface RelaxingImagesRepository {
+    fun getDogImages(): List<DogImage>
+}
