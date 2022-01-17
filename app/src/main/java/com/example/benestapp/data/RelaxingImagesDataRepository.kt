2b3ed47ee4ab392c20b08f2a0fa4ddfd6bfece81
laia <laia.igelmo.a@gmail.com>
2022-01-17 15:59:14 +0100

    Create getDogImages function inside relaxing images repositories.

--- app/src/main/java/com/example/benestapp/data/RelaxingImagesDataRepository.kt
+++ app/src/main/java/com/example/benestapp/data/RelaxingImagesDataRepository.kt
@@ -1,4 +1,15 @@
 package com.example.benestapp.data
 
-class RelaxingImagesDataRepository {
+import com.example.benestapp.domain.RelaxingImagesRepository
+import com.example.benestapp.domain.structure.DogImage
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RelaxingImagesDataRepository : RelaxingImagesRepository {
+    override fun getDogImages(): List<DogImage> {
+        val list = listOf<DogImage>()
+        val retrofit = Retrofit.Builder().baseUrl("https://dog.ceo/api/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        return list
+    }
 }
--- app/src/main/java/com/example/benestapp/domain/RelaxingImagesRepository.kt
+++ app/src/main/java/com/example/benestapp/domain/RelaxingImagesRepository.kt
@@ -1,5 +1,7 @@
 package com.example.benestapp.domain
 
+import com.example.benestapp.domain.structure.DogImage
+
 interface RelaxingImagesRepository {
     fun getDogImages(): List<DogImage>
 }
